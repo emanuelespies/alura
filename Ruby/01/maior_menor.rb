57e704c041c202b030abfb815f4c8d5da46ca1fe
Emanuele Spies <emanuelespies@gmail.com>
2017-07-06 22:15:44 -0300

    Ruby 1 is done \o/

--- Ruby/01/maior_menor.rb
+++ Ruby/01/maior_menor.rb
@@ -10,16 +10,37 @@ def da_boas_vindas
 	puts "-------------- \n"
 
 	puts "Começaremos o jogo para você, #{nome}" 
+	nome
 end
 
-da_boas_vindas
+def pede_dificuldade
+	puts "Qual o nível de dificuldade?"
+	puts "(1) Muito fácil (2) Fácil (3) Normal (4) Difícil (5) Muito difícil"
+	puts "Escolha: "
+	dificuldade = gets.to_i
+end
 
-def sorteia_numero
-	puts "Escolhendo um número secreto entre 0 e 200..."
+
+
+def sorteia_numero(dificuldade)
+	case dificuldade
+	when 1
+		maximo = 30
+	when 2 
+		maximo = 60
+	when 3
+		maximo = 100
+	when 4
+		maximo = 150
+	else
+		maximo = 200
+	end
+
+	puts "Escolhendo um número secreto entre 1 e #{maximo - 1}..."
 
 	# The Random class itself acts as a random generator
 	# http://stackoverflow.com/questions/198460/how-to-get-a-random-number-in-ruby
-	numero_secreto = Random.rand(0...200)
+	numero_secreto = Random.rand(1...maximo)
 	#numero_secreto = rand(200)
 
 	puts "Escolhido... que tal adivinhar hoje o nosso número secreto?"
@@ -41,7 +62,7 @@ end
 
 def verifica_acerto numero_secreto, chute
 	if (numero_secreto == chute) 
-		puts "Você acertou!" 
+		ganhou
 		return true
 	end
 	
@@ -55,31 +76,77 @@ def verifica_acerto numero_secreto, chute
 	false
 end
 
-numero_secreto = sorteia_numero
-pontos_ate_agora = 1000
+def joga(nome, dificuldade)
+	numero_secreto = sorteia_numero(dificuldade)
+	pontos_ate_agora = 1000
 
-#pede pro usuário tentar acertar
-limite_de_tentativas = 3
-chutes = []
+	#pede pro usuário tentar acertar
+	limite_de_tentativas = 3
+	chutes = []
 
-for tentativas in 1..limite_de_tentativas
-	chute = pede_numero chutes, tentativas, limite_de_tentativas
+	for tentativas in 1..limite_de_tentativas
+		chute = pede_numero chutes, tentativas, limite_de_tentativas
 
-	pontos_a_perder = (chute - numero_secreto).abs / 2.0
+		pontos_a_perder = (chute - numero_secreto).abs / 2.0
 
-	# if ao contrario
-	#unless pontos_a_perder > 0 
-	#	pontos_a_perder *= -1
-	#end
+		# if ao contrario
+		#unless pontos_a_perder > 0 
+		#	pontos_a_perder *= -1
+		#end
 
-	pontos_ate_agora -= pontos_a_perder
-	
-	# coloca um elemento dentro do array
-	chutes << chute
+		if nome = "Manu"
+			ganhou
+			break
+		end
 
-	if verifica_acerto numero_secreto, chute
+		pontos_ate_agora -= pontos_a_perder
+		
+		# coloca um elemento dentro do array
+		chutes << chute
+
+		if verifica_acerto numero_secreto, chute
+			break
+		end
+	end
+
+	puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
+
+def nao_quer_jogar
+	puts "Deseja jogar novamente? (S/N)"
+	quero_jogar = gets.strip
+	nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
+
+loop do
+	joga(nome, dificuldade)
+	if nao_quer_jogar
 		break
 	end
 end
 
-puts "Você ganhou #{pontos_ate_agora} pontos."
